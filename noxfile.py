0d8f9805206427e2a36d20c708ff2a171eec9c05
Sylvain MARIE <sylvain.marie@se.com>
2021-11-08 17:59:27 +0100

    Fixed mistake

--- noxfile.py
+++ noxfile.py
@@ -109,7 +109,7 @@ def tests(session: PowerSession, coverage, pkg_specs):
 
     # check that it can be imported even from a different folder
     # Important: do not surround the command into double quotes as in the shell !
-    session.run2('python', '-c', 'import os; os.chdir(\'./docs/\'); import %s' % pkg_name)
+    session.run('python', '-c', 'import os; os.chdir(\'./docs/\'); import %s' % pkg_name)
 
     # finally run all tests
     if not coverage:
